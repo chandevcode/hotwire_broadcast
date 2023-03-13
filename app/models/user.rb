9ff986ca11f6bdc96b1613f88bc3e57e3191397d
Putu Chandra <baguschandradp@gmail.com>
2023-03-13 10:02:32 +0800

    add online indicator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ApplicationRecord
   include ActionView::RecordIdentifier
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
-  after_create_commit -> { broadcast_prepend_to 'registrations', target: 'users' }
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
-  after_update_commit -> { broadcast_replace_to 'registrations', target: "#{dom_id self}_row" }
+  after_create_commit lambda {
+                        broadcast_prepend_to 'users_list', target: 'users'
+                      }
 
-  after_destroy_commit -> { broadcast_remove_to 'registrations', target: "#{dom_id self}_row" }
+  after_update_commit lambda {
+                        broadcast_replace_to 'users_list',
+                                             target: "#{dom_id self}_row"
+                      }
+
+  after_destroy_commit lambda {
+                         broadcast_remove_to 'users_list',
+                                             target: "#{dom_id self}_row"
+                       }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'site/index'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users,
+             controller: {
+               registrations: 'registrations',
+               sessions: 'sessions'
+             }
+  root 'site#index'
 end
